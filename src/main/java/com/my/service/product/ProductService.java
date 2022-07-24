4cf25dff4e72edc638d17f4a2b22556403c20cd3
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-07-24 16:47:41 +0200

    Add putProduct() method in ProductService. Add ProductServiceImpl class.

--- src/main/java/com/my/service/product/ProductService.java
+++ src/main/java/com/my/service/product/ProductService.java
@@ -2,12 +2,14 @@ package com.my.service.product;
 
 import com.my.entity.CPU;
 import com.my.entity.ProductFilterFormBean;
+import com.my.service.ServiceException;
 
 import java.util.List;
 
 public interface ProductService {
     CPU getProductById(int id);
-    void buyProduct(CPU cpu, int amount);
+    void buyProduct(CPU cpu, int amount) throws ServiceException;
+    void putProduct(CPU cpu, int amount);
     List<CPU> getProducts(ProductFilterFormBean bean, int pageSize, int pageCount);
     int getProductCount(ProductFilterFormBean bean);
 }
---
+++ src/main/java/com/my/service/product/impl/ProductServiceImpl.java
@@ -0,0 +1,69 @@
+package com.my.service.product.impl;
+
+import com.my.dao.manufacturer.ManufacturerDAO;
+import com.my.dao.mt.MemoryTypeDAO;
+import com.my.dao.product.ProductDAO;
+import com.my.entity.CPU;
+import com.my.entity.ProductFilterFormBean;
+import com.my.entity.dto.CPUDataTransferObject;
+import com.my.service.ServiceException;
+import com.my.service.product.ProductService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProductServiceImpl implements ProductService {
+    private final ProductDAO productDAO;
+    private final ManufacturerDAO manufacturerDAO;
+    private final MemoryTypeDAO memoryTypeDAO;
+
+    public ProductServiceImpl(ProductDAO productDAO, ManufacturerDAO manufacturerDAO, MemoryTypeDAO memoryTypeDAO) {
+        this.productDAO = productDAO;
+        this.manufacturerDAO = manufacturerDAO;
+        this.memoryTypeDAO = memoryTypeDAO;
+    }
+
+    @Override
+    public CPU getProductById(int id) {
+        CPUDataTransferObject cpuDataTransferObject = productDAO.getProductById(id);
+        return parseCPUDataTransferObject(cpuDataTransferObject);
+    }
+
+    @Override
+    public void buyProduct(CPU cpu, int amount) throws ServiceException {
+        int productAmount = productDAO.getProductAmount(cpu);
+        if (productAmount - amount < 0) {
+            throw new ServiceException("Cannot buy this amount of product");
+        }
+        productAmount -= amount;
+        productDAO.updateProductAmount(cpu, productAmount);
+    }
+
+    @Override
+    public void putProduct(CPU cpu, int amount) {
+        int productAmount = productDAO.getProductAmount(cpu) + amount;
+        productDAO.updateProductAmount(cpu, productAmount);
+    }
+
+    @Override
+    public List<CPU> getProducts(ProductFilterFormBean bean, int pageSize, int pageCount) {
+        List<CPUDataTransferObject> cpuDataTransferObjects = productDAO.getProducts(bean, pageSize, pageCount);
+        return cpuDataTransferObjects.stream()
+                .map(this::parseCPUDataTransferObject)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public int getProductCount(ProductFilterFormBean bean) {
+        return productDAO.getProductCount(bean);
+    }
+
+    private CPU parseCPUDataTransferObject(CPUDataTransferObject cpuDataTransferObject) {
+        CPU cpu = cpuDataTransferObject.getCpu();
+        String memoryType = memoryTypeDAO.getMemoryTypeById(cpuDataTransferObject.getMemoryTypeId());
+        String manufacturer = manufacturerDAO.getManufacturerById(cpuDataTransferObject.getManufacturerId());
+        cpu.setManufacturer(manufacturer);
+        cpu.setMemoryType(memoryType);
+        return cpu;
+    }
+}
