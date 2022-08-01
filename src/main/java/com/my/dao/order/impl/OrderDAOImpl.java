43dc2f9de63aea98c5ca2e8868c4b8276465fc03
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-08-01 14:40:22 +0300

    Add OrderedProductsDAO dependency to OrderService. OrderDAO writes order's id now.

--- src/main/java/com/my/dao/order/impl/OrderDAOImpl.java
+++ src/main/java/com/my/dao/order/impl/OrderDAOImpl.java
@@ -7,6 +7,7 @@ import com.my.entity.Order;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,8 +33,17 @@ public class OrderDAOImpl implements OrderDAO {
             preparedStatement.setString(index++, order.getAddress());
             preparedStatement.setInt(index, order.getUser().getId());
             preparedStatement.execute();
+            setId(connection, order);
         } catch (SQLException | DBException e) {
             logger.log(Level.SEVERE, "Cannot put order in database");
+        }
+    }
+
+    private void setId(Connection connection, Order order) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT LAST_INSERT_ID() FROM orders;");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next()){
+            order.setId(resultSet.getInt(BEGIN_INDEX));
         }
     }
 }
--- src/main/java/com/my/service/order/impl/OrderServiceImpl.java
+++ src/main/java/com/my/service/order/impl/OrderServiceImpl.java
@@ -1,18 +1,22 @@
 package com.my.service.order.impl;
 
 import com.my.dao.order.OrderDAO;
+import com.my.dao.order.OrderedProductsDAO;
 import com.my.entity.Order;
 import com.my.service.order.OrderService;
 
 public class OrderServiceImpl implements OrderService {
     private final OrderDAO orderDAO;
+    private final OrderedProductsDAO orderedProductsDAO;
 
-    public OrderServiceImpl(OrderDAO orderDAO){
+    public OrderServiceImpl(OrderDAO orderDAO, OrderedProductsDAO orderedProductsDAO) {
         this.orderDAO = orderDAO;
+        this.orderedProductsDAO = orderedProductsDAO;
     }
 
     @Override
     public void create(Order order) {
         orderDAO.put(order);
+        orderedProductsDAO.put(order.getOrderedProducts(), order.getId());
     }
 }
