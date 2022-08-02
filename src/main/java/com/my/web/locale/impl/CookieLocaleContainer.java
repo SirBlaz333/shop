d5853ce4d97aee0e84bb5958e1ffbe65f7fc7266
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-08-02 18:27:42 +0300

    Add LocaleContainer interface. Add CookieLocaleContainer and SessionLocaleContainer, which implement LocaleContainer.

---
+++ src/main/java/com/my/web/locale/impl/CookieLocaleContainer.java
@@ -0,0 +1,28 @@
+package com.my.web.locale.impl;
+
+import com.my.web.filter.LocaleFilter;
+import com.my.web.locale.LocaleContainer;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class CookieLocaleContainer implements LocaleContainer {
+    @Override
+    public Locale getLocale(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(LocaleFilter.LANGUAGE)) {
+                return new Locale(cookie.getValue());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+        Cookie cookie = new Cookie(LocaleFilter.LANGUAGE, locale.getLanguage());
+        response.addCookie(cookie);
+    }
+}
---
+++ src/main/java/com/my/web/locale/impl/SessionLocaleContainer.java
@@ -0,0 +1,20 @@
+package com.my.web.locale.impl;
+
+import com.my.web.filter.LocaleFilter;
+import com.my.web.locale.LocaleContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class SessionLocaleContainer implements LocaleContainer {
+    @Override
+    public Locale getLocale(HttpServletRequest request, HttpServletResponse response) {
+        return (Locale) request.getSession().getAttribute(LocaleFilter.LANGUAGE);
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+        request.getSession().setAttribute(LocaleFilter.LANGUAGE, locale);
+    }
+}
