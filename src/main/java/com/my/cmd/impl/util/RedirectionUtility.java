6c45f3131ed21a8abcf510376a9c91468e07a76c
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-08-06 11:54:57 +0300

    requestDispatcher.forward() was replaced with response.sendRedirect()

--- src/main/java/com/my/cmd/impl/util/RedirectionUtility.java
+++ src/main/java/com/my/cmd/impl/util/RedirectionUtility.java
@@ -9,9 +9,9 @@ public class RedirectionUtility {
     public static final String REDIRECT_URL = "redirectURL";
     public static final String ERROR_MESSAGE = "errorMessage";
 
-    public void showError(HttpServletRequest request, HttpServletResponse response, String pageToRedirect, String errorMessage) throws ServletException, IOException {
-        request.setAttribute(ERROR_MESSAGE, errorMessage);
-        request.getRequestDispatcher(pageToRedirect).forward(request, response);
+    public void showError(HttpServletRequest request, HttpServletResponse response, String pageToRedirect, String errorMessage) throws IOException {
+        String url = appendErrorMessage(pageToRedirect, errorMessage);
+        response.sendRedirect(url);
     }
 
     public String getRedirectUrl(HttpServletRequest request, String defaultURL) {
@@ -21,5 +21,9 @@ public class RedirectionUtility {
 
     public void setRedirectUrl(HttpServletRequest request) {
         request.setAttribute(REDIRECT_URL, request.getParameter(REDIRECT_URL));
+    }
+
+    private String appendErrorMessage(String URL, String paramValue) {
+        return URL + "&" + RedirectionUtility.ERROR_MESSAGE + "=" + paramValue + "?";
     }
 }
