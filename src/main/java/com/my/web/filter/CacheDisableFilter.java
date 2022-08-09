15a5c14135667f54ca959e255744adaf6ca35322
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-08-09 21:39:50 +0300

    Add CacheDisableFilter

---
+++ src/main/java/com/my/web/filter/CacheDisableFilter.java
@@ -0,0 +1,23 @@
+package com.my.web.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CacheDisableFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Cache-Control", "no-cache");
+        filterChain.doFilter(servletRequest, response);
+    }
+}
